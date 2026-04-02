071d75328b404cd0e8cc87d4c15a10b8c2abfc7b
Eph23 <ephraims.818@gmail.com>
2026-04-03 02:19:01 +0600

    REACT--COMPLETE-GUIDE - REACT ESSENTIALS - 04. Making Components Reusable with Props

--- 01. Essentials/react-essentials/src/App.jsx
+++ 01. Essentials/react-essentials/src/App.jsx
@@ -1,4 +1,5 @@
 import reactImg from "./assets/react-core-concepts.png";
+import componentsImg from "./assets/components.png";
 
 const reactDescriptions = ["Fundamental", "Crucial", "Core"];
 function genRandom(max) {
@@ -19,11 +20,31 @@ function Header() {
     );
 }
 
+function CoreConcept(props) {
+    return (
+        <li>
+            <img src={props.image} alt={props.title} />
+            <h3>{props.title}</h3>
+            <p>{props.description}</p>
+        </li>
+    );
+}
+
 function App() {
     return (
         <div>
             <Header />
             <main>
+                <section id="core-concepts">
+                    <h2>Core Concept</h2>
+                    <ul>
+                        <CoreConcept
+                            title="Components"
+                            description="The code UI building block"
+                            image={componentsImg}
+                        />
+                    </ul>
+                </section>
                 <h2>Time to get started!</h2>
             </main>
         </div>
