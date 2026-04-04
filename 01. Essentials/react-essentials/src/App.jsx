22c750542a8b2610aebacae390b6d3b018e7a084
Eph23 <ephraims.818@gmail.com>
2026-04-05 03:06:15 +0600

    REACT--COMPLETE-GUIDE - REACT ESSENTIALS - 08. The Special Children Prop

--- 01. Essentials/react-essentials/src/App.jsx
+++ 01. Essentials/react-essentials/src/App.jsx
@@ -1,6 +1,7 @@
 import Header from "./components/Header/Header.jsx";
 import CoreConcept from "./components/CoreConcept.jsx";
 import { CORE_CONCEPTS } from "./data";
+import TabButton from "./components/TabButton.jsx";
 
 function App() {
     return (
@@ -20,7 +21,15 @@ function App() {
                         <CoreConcept {...CORE_CONCEPTS[3]} />
                     </ul>
                 </section>
-                <h2>Time to get started!</h2>
+                <section id="examples">
+                    <h2>Examples</h2>
+                    <menu>
+                        <TabButton>Components</TabButton>
+                        <TabButton>JSX</TabButton>
+                        <TabButton>Props</TabButton>
+                        <TabButton>State</TabButton>
+                    </menu>
+                </section>
             </main>
         </div>
     );
