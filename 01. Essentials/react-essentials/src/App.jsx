76e28b134d4a16c32ae13705a3c5a1b925b65427
Eph23 <ephraims.818@gmail.com>
2026-04-07 21:40:20 +0600

    REACT--COMPLETE-GUIDE - REACT ESSENTIALS - 13. Outputting Data Based on State

--- 01. Essentials/react-essentials/src/App.jsx
+++ 01. Essentials/react-essentials/src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from "react";
 import Header from "./components/Header/Header.jsx";
 import CoreConcept from "./components/CoreConcept.jsx";
-import { CORE_CONCEPTS } from "./data";
+import { CORE_CONCEPTS } from "./data.js";
+import { EXAMPLES } from "./data.js";
 import TabButton from "./components/TabButton.jsx";
 
 function App() {
-    const [selectedTopic, setSelectedTopic] = useState("Please click a button");
+    const [selectedTopic, setSelectedTopic] = useState("components");
 
     function handleSelect(selectedButton) {
         setSelectedTopic(selectedButton);
@@ -44,7 +45,13 @@ function App() {
                             State
                         </TabButton>
                     </menu>
-                    {selectedTopic}
+                    <div id="tab-content">
+                        <h3>{EXAMPLES[selectedTopic].title}</h3>
+                        <p>{EXAMPLES[selectedTopic].description}</p>
+                        <pre>
+                            <code>{EXAMPLES[selectedTopic].code}</code>
+                        </pre>
+                    </div>
                 </section>
             </main>
         </div>
