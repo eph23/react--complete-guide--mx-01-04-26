7f1c0e033f5aa13412c74a35266a6069e6b946b0
Eph23 <ephraims.818@gmail.com>
2026-04-07 21:49:31 +0600

    REACT--COMPLETE-GUIDE - REACT ESSENTIALS - 14. Rendering Content Conditionally

--- 01. Essentials/react-essentials/src/App.jsx
+++ 01. Essentials/react-essentials/src/App.jsx
@@ -6,7 +6,7 @@ import { EXAMPLES } from "./data.js";
 import TabButton from "./components/TabButton.jsx";
 
 function App() {
-    const [selectedTopic, setSelectedTopic] = useState("components");
+    const [selectedTopic, setSelectedTopic] = useState();
 
     function handleSelect(selectedButton) {
         setSelectedTopic(selectedButton);
@@ -46,11 +46,17 @@ function App() {
                         </TabButton>
                     </menu>
                     <div id="tab-content">
-                        <h3>{EXAMPLES[selectedTopic].title}</h3>
-                        <p>{EXAMPLES[selectedTopic].description}</p>
-                        <pre>
-                            <code>{EXAMPLES[selectedTopic].code}</code>
-                        </pre>
+                        {!selectedTopic ? (
+                            <p>Please select a topic</p>
+                        ) : (
+                            <>
+                                <h3>{EXAMPLES[selectedTopic].title}</h3>
+                                <p>{EXAMPLES[selectedTopic].description}</p>
+                                <pre>
+                                    <code>{EXAMPLES[selectedTopic].code}</code>
+                                </pre>
+                            </>
+                        )}
                     </div>
                 </section>
             </main>
