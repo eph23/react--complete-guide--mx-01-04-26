98d19cb58b979eef12dd6bc9ca87cf320e5a38ef
Eph23 <ephraims.818@gmail.com>
2026-04-07 22:03:49 +0600

    REACT--COMPLETE-GUIDE - REACT ESSENTIALS - 16. Outputting List Data Dynamically

--- 01. Essentials/react-essentials/src/App.jsx
+++ 01. Essentials/react-essentials/src/App.jsx
@@ -19,14 +19,24 @@ function App() {
                 <section id="core-concepts">
                     <h2>Core Concept</h2>
                     <ul>
-                        <CoreConcept
+                        {CORE_CONCEPTS.map((item) => (
+                            /*  <CoreConcept
+                                key={item.title}
+                                title={item.title}
+                                description={item.description}
+                                image={item.image}
+                            /> */
+                            <CoreConcept key={item.title} {...item} />
+                        ))}
+                        {/* <CoreConcept
                             title={CORE_CONCEPTS[0].title}
                             description={CORE_CONCEPTS[0].description}
                             image={CORE_CONCEPTS[0].image}
                         />
                         <CoreConcept {...CORE_CONCEPTS[1]} />
                         <CoreConcept {...CORE_CONCEPTS[2]} />
-                        <CoreConcept {...CORE_CONCEPTS[3]} />
+                        <CoreConcept {...CORE_CONCEPTS[3]} /> 
+                        */}
                     </ul>
                 </section>
                 <section id="examples">
