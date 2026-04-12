70b46d600e9676196a55547c68e44658dc3ef96b
Eph23 <ephraims.818@gmail.com>
2026-04-13 04:53:14 +0600

    REACT--COMPLETE-GUIDE - REACT DEEP DIVE -- Tic-Tac-Toe - 18. Lifting Computed Values

--- 02. Deep Dive/tic-tac-toe/src/App.jsx
+++ 02. Deep Dive/tic-tac-toe/src/App.jsx
@@ -4,6 +4,12 @@ import GameBoard from "./components/GameBoard";
 import Log from "./components/Log";
 import { WINNING_COMBINATIONS } from "./winning-combinations.js";
 
+const initialGameBoard = [
+    [null, null, null],
+    [null, null, null],
+    [null, null, null],
+];
+
 function derivedActivePlayer(gameTurns) {
     let currentPlayer = "X";
 
@@ -18,6 +24,21 @@ function App() {
     const [gameTurns, setGameTurns] = useState([]);
 
     const activePlayer = derivedActivePlayer(gameTurns);
+
+    let gameBoard = initialGameBoard;
+
+    for (const turn of gameTurns) {
+        const { square, player } = turn;
+        const { row, col } = square;
+
+        gameBoard[row][col] = player;
+    }
+
+    for (const combination of WINNING_COMBINATIONS) {
+        const firstSquareSymbol;
+        const secondSquareSymbol;
+        const thirdSquareSymbol;
+    }
 
     function handleSelectSquare(rowIndex, colIndex) {
         setGameTurns((prevTurns) => {
@@ -52,7 +73,7 @@ function App() {
                 </ol>
                 <GameBoard
                     onSelectSquare={handleSelectSquare}
-                    turns={gameTurns}
+                    board={gameBoard}
                 />
             </div>
             <Log turns={gameTurns} />
