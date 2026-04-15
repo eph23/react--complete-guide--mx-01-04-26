a6751a1c117045c64963b7d463852b0cadf5ce82
Eph23 <ephraims.818@gmail.com>
2026-04-16 02:26:21 +0600

    REACT--COMPLETE-GUIDE - REACT ESSENTIALS PROJECT -- Investment-Calculator - 07. Outputting Content Conditionally

--- 03. Essentials [PROJECT]/investment-calculator/src/App.jsx
+++ 03. Essentials [PROJECT]/investment-calculator/src/App.jsx
@@ -11,6 +11,8 @@ function App() {
         duration: 10,
     });
 
+    const inputIsValid = userInput.duration >= 1;
+
     function handleChange(inputIdentifier, newValue) {
         setUserInput((prevUserInput) => {
             return {
@@ -24,7 +26,12 @@ function App() {
         <>
             <Header />
             <UserInput onChange={handleChange} userInput={userInput} />
-            <Results input={userInput} />
+            {!inputIsValid && (
+                <p class="center">
+                    Please enter a duration greater and than zero{" "}
+                </p>
+            )}
+            {inputIsValid && <Results input={userInput} />}
         </>
     );
 }
