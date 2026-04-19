66c873216fa67227b0e92c393d3fa50d64faa062
Eph23 <ephraims.818@gmail.com>
2026-04-19 21:14:52 +0600

    REACT--COMPLETE-GUIDE - STYLING COMPONENTS - 11. Introducing Tailwind CSS for React App Styling

--- 04. Styling/styling-react-app/src/App.jsx
+++ 04. Styling/styling-react-app/src/App.jsx
@@ -1,13 +1,13 @@
-import AuthInputs from './components/AuthInputs.jsx';
-import Header from './components/Header.jsx';
+import AuthInputs from "./components/AuthInputs.jsx";
+import Header from "./components/Header.jsx";
 
 export default function App() {
-  return (
-    <>
-      <Header />
-      <main>
-        <AuthInputs />
-      </main>
-    </>
-  );
+    return (
+        <>
+            <Header />
+            <main>
+                <AuthInputs />
+            </main>
+        </>
+    );
 }
