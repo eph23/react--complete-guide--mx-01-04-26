d4fb3add48b8f4464a2902bba824cc86ca711508
Eph23 <ephraims.818@gmail.com>
2026-04-24 01:46:34 +0600

    REACT--COMPLETE-GUIDE - REFS AND PORTALS - 08. Adding a Modal Component

---
+++ 06. Refs and Portals/refs-andportals/src/components/ResultModal.jsx
@@ -0,0 +1,18 @@
+function ResultModal({ result, targetTime }) {
+    return (
+        <dialog className="result-modal" >
+            <h2>You {result}</h2>
+            <p>
+                The target time was <strong>{targetTime} seconds.</strong>
+            </p>
+            <p>
+                You stopped the timer with <strong>X</strong> seconds left
+            </p>
+            <form method="dialog">
+                <button>Close</button>
+            </form>
+        </dialog>
+    );
+}
+
+export default ResultModal;
